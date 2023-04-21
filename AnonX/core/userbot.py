efcc431e43cbcaed1b70fe337ed55da8cf24d3f8
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-04-21 12:34:00 +0530

    userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
-                await self.one.join_chat("FallenAssociation")
+                await self.one.join_chat("TKS_CHAT_OFFICIAL")
+                await self.one.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(1)
@@ -78,9 +78,9 @@ class Userbot(Client):
                 sys.exit()
         if config.STRING2:
             await self.two.start()
-            try:
-                await self.two.join_chat("DevilsHeavenMF")
-                await self.two.join_chat("FallenAssociation")
+            try:  
+                await self.two.join_chat("TKS_CHAT_OFFICIAL")
+                await self.two.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
-                await self.three.join_chat("FallenAssociation")
+                await self.three.join_chat("TKS_CHAT_OFFICIAL")
+                await self.three.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
-                await self.four.join_chat("FallenAssociation")
+                await self.four.join_chat("TKS_CHAT_OFFICIA")
+                await self.four.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
-                await self.five.join_chat("FallenAssociation")
+                await self.five.join_chat("TKS_CHAT_OFFICIAL")
+                await self.five.join_chat("TKS_COME")
             except:
                 pass
             assistants.append(5)
