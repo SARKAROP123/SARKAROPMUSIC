220d67b30feb930f194efc71998c095be29fb014
SARKAROP123 <111958063+SARKAROP123@users.noreply.github.com>
2023-04-21 12:24:54 +0530

    bot.py

--- AnonX/core/bot.py
+++ AnonX/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class AnonXBot(Client):
+class SᴀʀᴋᴀʀXBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "AnonXMusic",
+            "💥𝗦𝗔𝗥𝗞𝗔𝗥X𝗠𝗨𝗦𝗜𝗖💥",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
